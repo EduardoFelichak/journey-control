beb50099fb5b40aa7d9d5f209219ee3504fae9ab
Eduardo Felichak <100370989+EduardoFelichak@users.noreply.github.com>
2025-01-16 08:27:31 -0300

    Secure Adjust

--- journey_control/DTOs/CustomFieldDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace journey_control.DTOs
-{
-    public class CustomFieldDto
-    {
-        public int Id { get; set; }
-        public string Value { get; set; }
-    }
-}
--- journey_control/Models/Activity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace journey_control.Models
-{
-    public class Activity
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
